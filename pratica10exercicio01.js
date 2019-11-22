1814926870ff90b1299c6c3bee804cd9f5147cc1
Douglas Ramos <55302738+douglas-rr@users.noreply.github.com>
2019-11-22 01:36:34 -0300

    Add files via upload

---
+++ pratica10exercicio01.js
@@ -0,0 +1,145 @@
+function orcamentoRio () {
+    if (document.getElementById ("orcamentoRio").style.display == "none") {
+        document.getElementById ("orcamentoRio").style.display = "";
+    } else {
+        document.getElementById ("orcamentoRio").style.display = "none";
+    }
+}
+
+function orcamentoSp () {
+    if (document.getElementById ("orcamentoSp").style.display == "none") {
+        document.getElementById ("orcamentoSp").style.display = "";
+    } else {
+        document.getElementById ("orcamentoSp").style.display = "none";
+    }
+}  
+
+function orcamentoBlumenau () {
+    if (document.getElementById ("orcamentoBlumenau").style.display == "none") {
+        document.getElementById ("orcamentoBlumenau").style.display = "";
+    } else {
+        document.getElementById ("orcamentoBlumenau").style.display = "none";
+    }
+}  
+
+function orcamentoBrasilia () {
+    if (document.getElementById ("orcamentoBrasilia").style.display == "none") {
+        document.getElementById ("orcamentoBrasilia").style.display = "";
+    } else {
+        document.getElementById ("orcamentoBrasilia").style.display = "none";
+    }
+}  
+
+
+function orcamentoCuritiba () {
+    if (document.getElementById ("orcamentoCuritiba").style.display == "none") {
+        document.getElementById ("orcamentoCuritiba").style.display = "";
+    } else {
+        document.getElementById ("orcamentoCuritiba").style.display = "none";
+    }
+}  
+
+
+function orcamentoFernando () {
+    if (document.getElementById ("orcamentoFernando").style.display == "none") {
+        document.getElementById ("orcamentoFernando").style.display = "";
+    } else {
+        document.getElementById ("orcamentoFernando").style.display = "none";
+    }
+}  
+
+function orcamentoFoz () {
+    if (document.getElementById ("orcamentoFoz").style.display == "none") {
+        document.getElementById ("orcamentoFoz").style.display = "";
+    } else {
+        document.getElementById ("orcamentoFoz").style.display = "none";
+    }
+}  
+
+function orcamentoJalapao () {
+    if (document.getElementById ("orcamentoJalapao").style.display == "none") {
+        document.getElementById ("orcamentoJalapao").style.display = "";
+    } else {
+        document.getElementById ("orcamentoJalapao").style.display = "none";
+    }
+}  
+
+function orcamentoJeri () {
+    if (document.getElementById ("orcamentoJeri").style.display == "none") {
+        document.getElementById ("orcamentoJeri").style.display = "";
+    } else {
+        document.getElementById ("orcamentoJeri").style.display = "none";
+    }
+}  
+
+function orcamentoMaceio () {
+    if (document.getElementById ("orcamentoMaceio").style.display == "none") {
+        document.getElementById ("orcamentoMaceio").style.display = "";
+    } else {
+        document.getElementById ("orcamentoMaceio").style.display = "none";
+    }
+}  
+
+function orcamentoManaus () {
+    if (document.getElementById ("orcamentoManaus").style.display == "none") {
+        document.getElementById ("orcamentoManaus").style.display = "";
+    } else {
+        document.getElementById ("orcamentoManaus").style.display = "none";
+    }
+}  
+
+function orcamentoLencol () {
+    if (document.getElementById ("orcamentoLencol").style.display == "none") {
+        document.getElementById ("orcamentoLencol").style.display = "";
+    } else {
+        document.getElementById ("orcamentoLencol").style.display = "none";
+    }
+}  
+
+function orcamentoNatal () {
+    if (document.getElementById ("orcamentoNatal").style.display == "none") {
+        document.getElementById ("orcamentoNatal").style.display = "";
+    } else {
+        document.getElementById ("orcamentoNatal").style.display = "none";
+    }
+}  
+
+function orcamentoPorto () {
+    if (document.getElementById ("orcamentoPorto").style.display == "none") {
+        document.getElementById ("orcamentoPorto").style.display = "";
+    } else {
+        document.getElementById ("orcamentoPorto").style.display = "none";
+    }
+}  
+
+function orcamentoRecife () {
+    if (document.getElementById ("orcamentoRecife").style.display == "none") {
+        document.getElementById ("orcamentoRecife").style.display = "";
+    } else {
+        document.getElementById ("orcamentoRecife").style.display = "none";
+    }
+}  
+
+function orcamentoSalvador () {
+    if (document.getElementById ("orcamentoSalvador").style.display == "none") {
+        document.getElementById ("orcamentoSalvador").style.display = "";
+    } else {
+        document.getElementById ("orcamentoSalvador").style.display = "none";
+    }
+}  
+
+function orcamentoSantos () {
+    if (document.getElementById ("orcamentoSantos").style.display == "none") {
+        document.getElementById ("orcamentoSantos").style.display = "";
+    } else {
+        document.getElementById ("orcamentoSantos").style.display = "none";
+    }
+}  
+
+function orcamentoVitoria () {
+    if (document.getElementById ("orcamentoVitoria").style.display == "none") {
+        document.getElementById ("orcamentoVitoria").style.display = "";
+    } else {
+        document.getElementById ("orcamentoVitoria").style.display = "none";
+    }
+} 
